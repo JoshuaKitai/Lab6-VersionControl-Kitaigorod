c592ce14fe06f7e2639171a04f4c68caeb710bf1
BuildTools <unconfigured@null.spigotmc.org>
2024-03-19 13:46:19 -0400

    Added decode function

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ def encode(password):
             res += str(int(char) + 3)
     return res
 
+def decode(password):
+    res = ''
+    for char in password:
+        if int(char) == 0 or int(char) == 1 or int(char) == 2:
+            res += str(int(char) + 7)
+        else:
+            res += str(int(char) - 3)
+    return res
 
 if __name__ == '__main__':
     i = 0
